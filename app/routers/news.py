8fff38276eee1bfc389b00ea2557901a3b76f827
Gulsara Najimova <gulsaranajimova@Gulsaras-MacBook-Pro.local>
2023-10-24 01:24:13 +0900

    Added news schema and news router

--- app/routers/news.py
+++ app/routers/news.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter
+from typing import Any
+from fastapi import APIRouter, Depends
+
+from app.schemas.news_schema import BaseNews, SearchNews
 
 
 news_router = APIRouter(
     prefix="/news",
     tags=["news"],
 )
+
+@news_router.post("/")
+async def news(news: BaseNews):
+    return {"news": news}
+
+@news_router.get("/search-news/{params}")
+async def search_news(params: Any = Depends(SearchNews)):
+    return {"news_found": params}
---
+++ app/schemas/news_schema.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from pydantic import BaseModel
+import pydantic
+
+
+class BaseNews(BaseModel):
+    title: str
+    author: str
+    source: str
+    description: str
+    url: pydantic.AnyHttpUrl
+    publishedAt: datetime
+
+
+class PriorityType(str, Enum):
+    EVERYTHING = "/everything"
+    TOP_HEADLINES = "/top-headlines"
+
+class SearchNews(BaseModel):
+    keyword: Optional[List[str]]
+    country: Optional[str]
+    priority: PriorityType
