2e4497a8a21da62a8c6647c3f538dd10b0ea4a33
Gulsara Najimova <gulsaranajimova@Gulsaras-MacBook-Pro.local>
2023-10-27 12:23:10 +0900

    add dependencies.py

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,52 +1,42 @@
+from dependency_injector.wiring import Provide, inject
 from fastapi import Depends
 from jose import jwt
 from pydantic import ValidationError
-from sqlalchemy.orm import Session
 
-from app.core import security, crud
+from app.core.config import configs
+from app.core.containers import Container
 from app.core.exceptions import AuthError
-from app.core.database import SessionLocal
-from app.schemas import Payload, UserSchema
-from app.models import UserType
+from app.core.security import ALGORITHM, JWTBearer
+from app.models.users_model import UserModel
+from app.schemas.auth_schema import Payload
+from app.services.user_service import UserService
 
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
+@inject
 def get_current_user(
-        db: Session = Depends(get_db), 
-        token: str = Depends(security.JWTBearer())
-    ):
+    token: str = Depends(JWTBearer()),
+    service: UserService = Depends(Provide[Container.user_service]),
+) -> UserModel:
     try:
-        payload = jwt.decode(
-            token, 
-            security.SECRET_KEY, 
-            algorithms = security.ALGORITHM)
+        payload = jwt.decode(token, configs.SECRET_KEY, algorithms=ALGORITHM)
         token_data = Payload(**payload)
-    except (jwt.JWTError, ValidationError): 
+    except (jwt.JWTError, ValidationError):
         raise AuthError("Could not validate credentials")
-    
-    current_user = crud.get_user_by_id(db, token_data.id)
- 
+    current_user: UserModel = service.get_by_id(token_data.id)
     if not current_user:
         raise AuthError("User not found")
     return current_user
 
 
-def get_current_active_user(current_user: UserSchema = Depends(get_current_user)):
+def get_current_active_user(current_user: UserModel = Depends(get_current_user)) -> UserModel:  # noqa: E501
     if not current_user.is_active:
-        raise AuthError("Inactive User")
+        raise AuthError("Inactive user")
     return current_user
 
 
-def get_current_superuser(current_user: UserSchema = Depends(get_current_user)):
+def get_current_superuser(current_user: UserModel = Depends(get_current_user)) -> UserModel:  # noqa: E501
     if not current_user.is_active:
-        raise AuthError("Inactive User")
-    if current_user.type != UserType.ADMIN:
-        raise AuthError("Not a superuser")
+        raise AuthError("Inactive user")
+    if not current_user.is_superuser:
+        raise AuthError("It's not a super user")
     return current_user
